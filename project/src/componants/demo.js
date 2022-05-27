1da6075f400e11f7cd0aa22f7b3f76e01b2c1988
jenishadholariya <jenishadholariya5@gmail.com>
2022-05-27 15:36:48 +0530

    28-05

---
+++ project/src/componants/demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                hello world
+            </div>
+        );
+    }
+}
+
+export default Demo;
