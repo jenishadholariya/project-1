e193fb159d42974737ef4a8b09d5f948384e95aa
jenishadholariya <jenishadholariya5@gmail.com>
2022-05-27 16:02:23 +0530

    27-05-22-demo

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Demo from './componants/demo';
 function App() {
-
   return (
-    <Demo/>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React read
+        </a>
+      </header>
+    </div>
   );
 }
 
