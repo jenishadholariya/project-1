43e8cca08bd597ed5a067eddafc780e29e2c0d6d
jenishadholariya <jenishadholariya5@gmail.com>
2022-06-01 11:01:09 +0530

    31-5-22

--- project/src/App.js
+++ project/src/App.js
@@ -1,27 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-import Demo from './componants/demo';
 
-<Demo/>
+import './App.css';
+import Demo from './component/Demo';
+
 function App() {
   
+  let data=[
+    {
+      id: 101,
+      name: 'Abacavir',
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
+      status: true
+    },
+    {
+      id: 102,
+      name: 'Eltrombopag',
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true
+    },
+    {
+      id: 103,
+      name: 'Meloxicam',
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
+      status: false
+    },
+    {
+      id: 104,
+      name: 'Allopurinol',
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
+      status: true
+    },
+    {
+      id: 105,
+      name: 'Phenytoin',
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false
+    }
+  ];
+  
+  
+  console.log(data);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React read
-        </a>
-      </header>
-      
+    <div>
+      <Demo/>
     </div>
   );
 }
