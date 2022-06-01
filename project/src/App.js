d334823eaa639a58f24306f4b99039cd1b5d68cc
jenishadholariya <jenishadholariya5@gmail.com>
2022-06-01 15:42:06 +0530

    circulate

--- project/src/App.js
+++ project/src/App.js
@@ -4,42 +4,87 @@ import Demo from './component/Demo';
 
 function App() {
   
-  // let arr=[10,20,30,40,50];
+  let data=[
+    {
+      id: 101,
+      name: 'Abacavir',
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
+      status: true
+    },
+    {
+      id: 102,
+      name: 'Eltrombopag',
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true
+    },
+    {
+      id: 103,
+      name: 'Meloxicam',
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
+      status: false
+    },
+    {
+      id: 104,
+      name: 'Allopurinol',
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
+      status: true
+    },
+    {
+      id: 105,
+      name: 'Phenytoin',
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false
+    }
+  ];
+  
+  let fdata=data.filter((d,i) => d.status === true && d.expiry>=2022);
 
-  // let arr2=[...arr];  
-  // console.log(arr2);
+  let ans=fdata.reduce((acc,d,i) => acc + d.price ,0);
 
-  // let arr3=[99,...arr,100];
-  // console.log(arr3);
-
-  // let [a,b,c,d,e]=arr;
-  // console.log(e);
-
-  let obj={
-    id:'101',
-    name:'jenisha'
-  };
-
-  let obj2={...obj};
-  console.log(obj);
-
-  let obj3={
-    ...obj,
-    id:"102",
-    name:"jeni",
-    location:"surat",
-    
-  };
-  console.log(obj3);
-
-  let [id,name,location]=obj3;
-  console.log(id);
-
+  console.log(ans);
   return (
     <div>
+      <table border="1">
+        <th>Id</th>
+        <th>Name</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        <th>Expiry</th>
+        <th>Status</th>
+        <th>Total</th>
 
+        {
+          fdata.map((d,i) => {
+            let {id,name,quantity,price,expiry,status}=d;
+
+            
+              return(
+              <tr key={i}>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{quantity}</td>
+              <td>{price}</td>
+              <td>{expiry}</td>
+              <td>{status.toString()}</td>
+              {i===0?<td rowSpan={2}>{ans}</td>:null}
+            </tr>
+            )
+          })
+        }
+      </table>
     </div>
   );
 }
+
 
 export default App;
