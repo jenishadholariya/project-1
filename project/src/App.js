31f7faa080bd00b39ed5db45daf900b52ada3130
jenishadholariya <jenishadholariya5@gmail.com>
2022-06-01 15:59:44 +0530

    project

--- project/src/App.js
+++ project/src/App.js
@@ -3,80 +3,73 @@ import './App.css';
 import Demo from './component/Demo';
 
 function App() {
-  
   let data=[
     {
-      id: 101,
-      name: 'Abacavir',
-      quantity: 25,
-      price: 150,
-      expiry: 2022,
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
       status: true
     },
     {
-      id: 102,
-      name: 'Eltrombopag',
-      quantity: 90,
-      price: 550,
-      expiry: 2021,
-      status: true
-    },
-    {
-      id: 103,
-      name: 'Meloxicam',
-      quantity: 85,
-      price: 450,
-      expiry: 2025,
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
       status: false
     },
     {
-      id: 104,
-      name: 'Allopurinol',
-      quantity: 50,
-      price: 600,
-      expiry: 2023,
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
       status: true
     },
     {
-      id: 105,
-      name: 'Phenytoin',
-      quantity: 63,
-      price: 250,
-      expiry: 2021,
-      status: false
-    }
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
   ];
   
-  let fdata=data.filter((d,i) => d.status === true && d.expiry>=2022);
+  let fdata=data.filter((d,i)=>d.status===true && d.salary>=10000);
 
-  let ans=fdata.reduce((acc,d,i) => acc + d.price ,0);
+  let ans=fdata.reduce((acc,d,i)=>acc+d.salary+d.bonus,0)
 
   console.log(ans);
   return (
     <div>
       <table border="1">
-        <th>Id</th>
         <th>Name</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Expiry</th>
+        <th>Age</th>
+        <th>Salary</th>
+        <th>Bonus</th>
         <th>Status</th>
         <th>Total</th>
+        <th>Main-Total</th>
 
         {
           fdata.map((d,i) => {
-            let {id,name,quantity,price,expiry,status}=d;
 
-            
+            let {name,age,salary,bonus,status}=d;
               return(
-              <tr key={i}>
-              <td>{id}</td>
+              <tr>
               <td>{name}</td>
-              <td>{quantity}</td>
-              <td>{price}</td>
-              <td>{expiry}</td>
+              <td>{age}</td>
+              <td>{salary}</td>
+              <td>{bonus}</td>
               <td>{status.toString()}</td>
-              {i===0?<td rowSpan={2}>{ans}</td>:null}
+              <td>{d.salary+d.bonus}</td>
+              <td>{ans}</td>
             </tr>
             )
           })
