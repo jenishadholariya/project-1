267c64ea5fada33242a681a04b30f9ccfe479ffb
jenishadholariya <jenishadholariya5@gmail.com>
2022-06-01 13:02:31 +0530

    employ-data

--- project/src/App.js
+++ project/src/App.js
@@ -5,68 +5,62 @@ function App() {
   
   let data=[
     {
-      id: 101,
-      name: 'Abacavir',
-      quantity: 25,
-      price: 150,
-      expiry: 2022,
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
       status: true
     },
     {
-      id: 102,
-      name: 'Eltrombopag',
-      quantity: 90,
-      price: 550,
-      expiry: 2021,
-      status: true
-    },
-    {
-      id: 103,
-      name: 'Meloxicam',
-      quantity: 85,
-      price: 450,
-      expiry: 2025,
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
       status: false
     },
     {
-      id: 104,
-      name: 'Allopurinol',
-      quantity: 50,
-      price: 600,
-      expiry: 2023,
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
       status: true
     },
     {
-      id: 105,
-      name: 'Phenytoin',
-      quantity: 63,
-      price: 250,
-      expiry: 2021,
-      status: false
-    }
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
   ];
   
 
   return (
     <div>
       <table border="1">
-        <th>Id</th>
         <th>Name</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Expiry</th>
+        <th>Age</th>
+        <th>Salary</th>
+        <th>Bonus</th>
         <th>Status</th>
 
         {
           data.map((d,i) => {
-            let {id,name,quantity,price,expiry,status}=d;
+
+            let {name,age,salary,bonus,status}=d;
               return(
               <tr>
-              <td>{id}</td>
               <td>{name}</td>
-              <td>{quantity}</td>
-              <td>{price}</td>
-              <td>{expiry}</td>
+              <td>{age}</td>
+              <td>{salary}</td>
+              <td>{bonus}</td>
               <td>{status.toString()}</td>
             </tr>
             )
