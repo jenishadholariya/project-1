ba75188b7941bf784f79f5a21fd513ca97aa32f8
jenishadholariya <jenishadholariya5@gmail.com>
2022-06-06 16:13:58 +0530

    component-life-cycle-class

--- project/src/App.js
+++ project/src/App.js
@@ -3,12 +3,14 @@ import City from './Container/City';
 import Cityfun from './Container/Cityfun';
 import Country from './Container/Country';
 import Countryfun from './Container/Countryfun';
+import Time from './Time/Time';
 
 function App(props){
 
   return (
     <div>
-      <Country />
+      <Time />
+      {/* <Country /> */}
       {/* <Countryfun /> */}
 
     </div>
